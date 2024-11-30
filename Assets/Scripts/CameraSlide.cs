ddb31ee53266966769a64a5b64043acc8f462d42
ColtonDSnook <144720034+ColtonDSnook@users.noreply.github.com>
2024-11-30 16:09:55 -0400

    Added settings border and camera move script

---
+++ Assets/Scripts/CameraSlide.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CameraSlide : MonoBehaviour
+{
+    public Transform cameraTrans;
+    public Vector3 startPos;
+    public Vector3 endPos;
+    public float moveSpeed = 1.0f;
+
+    private bool moved = false;
+
+    public void ToggleCam()
+    {
+        StopAllCoroutines();
+        if (moved)
+        {
+            StartCoroutine(MoveCam(startPos));
+        }
+        else
+        {
+            StartCoroutine(MoveCam(endPos));
+        }
+        moved = !moved;
+    }
+
+    private IEnumerator MoveCam(Vector3 targetPostion)
+    {
+        while (Vector3.Distance(cameraTrans.position, targetPostion) > 0.1f)
+        {
+            cameraTrans.position = Vector3.Lerp(cameraTrans.position, targetPostion, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        cameraTrans.position = targetPostion;
+    }
+}
